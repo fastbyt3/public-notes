9e024ae972bd409e73f95c54461a52ed1e15d89b
Fastbyt3 <aldeerayan@gmail.com>
2023-01-26 22:25:09 +0530

    1674752109

--- commit.sh
+++ commit.sh
@@ -2,3 +2,10 @@ git add .
 git commit -m "$(date +%s)"
 git push
 echo "[!] Done...."
+
+cp -r ~/Documents/work-notes/* ~/Documents/notes/Programming/
+cd ~/Documents/notes
+git add .
+git commit -m "$(date +%s)"
+git push
+echo "[!] Done"
